23d575c52357869ad261841d291851c3697cb0ff
touyou <fujiyou1121@gmail.com>
2018-03-28 08:59:08 +0900

    UXを改善

--- Wasestagram/TextViewController.swift
+++ Wasestagram/TextViewController.swift
@@ -9,35 +9,19 @@
 import UIKit
 
 class TextViewController: UIViewController {
-    @IBOutlet weak var textView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-        if let viewController = segue.destination as? ViewController {
-            
-            viewController.text = textView.text
-        }
-    }
-    
-    @IBAction func next(_ sender: Any) {
-        performSegue(withIdentifier: "next", sender: nil)
-    }
-    
 }
